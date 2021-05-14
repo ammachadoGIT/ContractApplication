0d5f394614a10130f66539eae61a8db329dfaba9
Andre Machado <andre.machado@nymity.com>
2021-05-13 21:58:19 -0400

    remove unused class

--- ContractApplication/Models/Relationship.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ContractApplication.Models
-{
-    public class Relationship
-    {
-        public Contractor Person1Id { get; set; }
-
-        public int Person2Id { get; set; }
-    }
-}
