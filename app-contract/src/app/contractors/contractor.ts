03bf22f3d2a5c9170769b678bdade801eca93695
Andre Machado <andre.machado@nymity.com>
2021-05-15 18:49:08 -0400

    fix lint issues

--- app-contract/src/app/contractors/contractor.ts
+++ app-contract/src/app/contractors/contractor.ts
@@ -1,11 +1,11 @@
-import { HealthStatus } from "./HealthStatus";
-import { ContractorType } from "./ContractorType";
+import { HealthStatus } from './HealthStatus';
+import { ContractorType } from './ContractorType';
 
 export interface Contractor {
   id: number;
   name: string;
   address: string;
   phoneNumber: string;
-  contractorType: ContractorType,
-  healthStatus: HealthStatus,
+  contractorType: ContractorType;
+  healthStatus: HealthStatus;
 }
--- app-contract/src/app/shared/BaseService.ts
+++ app-contract/src/app/shared/BaseService.ts
@@ -2,22 +2,19 @@ import { MessageService } from '../message.service';
 import { Observable, of } from 'rxjs';
 
 export class BaseService {
-    constructor(
-        private messageService: MessageService) { }
+  constructor(
+    private messageService: MessageService) { }
 
-    /**
+  /**
    * Handle Http operation that failed.
    * Let the app continue.
    * @param operation - name of the operation that failed
    * @param result - optional value to return as the observable result
    */
-    protected handleError<T>(operation = 'operation', result?: T) {
+  protected handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
 
-      // TODO: send the error to remote logging infrastructure
       console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
       this.log(`${operation} failed: ${error.message}`);
 
       // Let the app keep running by returning an empty result.
