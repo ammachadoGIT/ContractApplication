26dca8119e5f05c024b32d5165486823a3e97a6b
Andre Machado <andre.machado@nymity.com>
2021-05-16 00:15:32 -0400

    fix contractor detail page

--- app-contract/src/app/contractor-detail/contractor-detail.component.ts
+++ app-contract/src/app/contractor-detail/contractor-detail.component.ts
@@ -8,28 +8,52 @@ import { ContractorService } from '../contractors/contractor.service';
 @Component({
   selector: 'app-contractor-detail',
   templateUrl: './contractor-detail.component.html',
-  styleUrls: [ './contractor-detail.component.css' ]
+  styleUrls: ['./contractor-detail.component.css']
 })
 export class ContractorDetailComponent implements OnInit {
   contractor: Contractor;
+  isReadOnly: boolean;
 
   constructor(
     private route: ActivatedRoute,
     private contractorService: ContractorService,
     private location: Location
-  ) {}
+  ) { }
 
   ngOnInit(): void {
-    this.getContractor();
+    const id = +this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.getContractor(id)
+      this.isReadOnly = true;
+    }
+    else {
+      this.clearContractor();
+      this.isReadOnly = false;
+    }
   }
 
-  getContractor(): void {
-    const id = +this.route.snapshot.paramMap.get('id');
+  add(contractor: Contractor): void {
+    this.contractorService.addContractor(contractor)
+      .subscribe(_ => this.clearContractor());
+  }
+
+  getContractor(id: number): void {
     this.contractorService.getContractor(id)
       .subscribe(contractor => this.contractor = contractor);
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  clearContractor() {
+    this.contractor = {
+      id: 0,
+      name: '',
+      address: '',
+      phoneNumber: '',
+      healthStatus: null,
+      contractorType: null
+    };
   }
 }
